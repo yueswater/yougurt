a603192a49be17a5f60b211d948f229d5166e32d
xiaolong70701 <xiaolong70701@gmail.com>
2025-07-05 03:26:36 +0800

    chore: auto-format via pre-commit

---
+++ src/web/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
