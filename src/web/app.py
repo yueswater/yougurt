f42cbcc5bcb22532a9ee6e05a59425bb5df2f19c
xiaolong70701 <xiaolong70701@gmail.com>
2025-07-10 13:21:26 +0800

    feat: add liff login page

--- src/web/app.py
+++ src/web/app.py
@@ -1,9 +1,29 @@
 import logging
+import os
 from datetime import datetime
 from uuid import uuid4
 
 import requests
-from flask import Blueprint, render_template, request
+from dotenv import load_dotenv
+from flask import Blueprint, Flask, render_template, request
+
+from flask_session import Session
+from src.web.views.liff import liff_bp
+
+dotenv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.env"))
+load_dotenv(dotenv_path)
+
+
+# Load LINE redirect URI after dotenv
+LINE_REDIRECT_URI = os.getenv("LINE_REDIRECT_URI")
+print("LINE_REDIRECT_URI =", LINE_REDIRECT_URI)
+
+# Import after env loaded
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "ringo123")
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -37,7 +57,7 @@ def bind():
     phone = request.args.get("phone")
 
     if not line_id or not name or not phone:
-        return render_template("bind_fail.html", message="參數不完整")
+        return render_template("bind_fail.html", message="Missing parameters")
 
     try:
         res = requests.post(
@@ -52,14 +72,24 @@ def bind():
                 "remain_delivery": 0,
                 "remain_volume": 0,
                 "prepaid": 0,
+                "valid_member": "TRUE",
             },
         )
 
-        logging.debug("狀態碼：", res.status_code)
-        logging.debug("錯誤內容：", res.text)
+        logging.debug(f"Status code: {res.status_code}")
+        logging.debug(f"Response text: {res.text}")
         if res.ok:
             return render_template("bind_success.html", name=name)
         else:
-            return render_template("bind_fail.html", message="綁定失敗，請確認是否已註冊過")
+            return render_template(
+                "bind_fail.html", message="Binding failed, maybe already registered"
+            )
     except Exception as e:
-        return render_template("bind_fail.html", message=f"伺服器錯誤：{e}")
+        return render_template("bind_fail.html", message=f"Server error: {e}")
+
+
+app.register_blueprint(web_bp)
+app.register_blueprint(liff_bp)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5500, debug=True)
