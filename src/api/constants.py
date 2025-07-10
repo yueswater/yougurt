401bd997904836d932df41d7917cbd345e82ae11
xiaolong70701 <xiaolong70701@gmail.com>
2025-07-10 14:51:26 +0800

    chore: add unused module API

--- src/api/constants.py
+++
@@ -1,1 +0,0 @@
-Messages = {"USER_NOT_FOUND": "會員不存在", "ERROR": "{error_type}時發生錯誤"}
--- src/api/utils/transform_member.py
+++
@@ -1,6 +0,0 @@
-from src.api.schemas.member_schema import MemberIn
-from src.models.member import Member
-
-
-def to_domain_member(member_in: MemberIn) -> Member:
-    return Member.from_dict(member_in.model_dump())
