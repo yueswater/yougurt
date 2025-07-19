34ed872e7d652c55670a48118210f3c7cb53aebf
xiaolong70701 <xiaolong70701@gmail.com>
2025-07-19 22:51:22 +0800

    feat: 啟動 linebot 婊子

--- src/utils/sheet_client.py
+++ src/utils/sheet_client.py
@@ -1,11 +1,13 @@
+import json
 import os
 
 import gspread
 from dotenv import load_dotenv
+from google.oauth2.service_account import Credentials
 
 load_dotenv()
 
-SERVICE_ACCOUNT_PATH = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON_PATH")
+SERVICE_ACCOUNT_JSON = os.getenv("SERVICE_ACCOUNT_JSON")
 SHEET_ID = os.getenv("GOOGLE_SHEET_ID")
 
 # global cache
@@ -17,7 +19,14 @@ def get_worksheet(sheet_name: str, force_reload: bool = False):
     global _gc, _spreadsheet
 
     if force_reload or _gc is None or _spreadsheet is None:
-        _gc = gspread.service_account(filename=SERVICE_ACCOUNT_PATH)
+        if not SERVICE_ACCOUNT_JSON:
+            raise RuntimeError("Missing SERVICE_ACCOUNT_JSON env variable")
+
+        info = json.loads(SERVICE_ACCOUNT_JSON)
+        creds = Credentials.from_service_account_info(
+            info, scopes=["https://www.googleapis.com/auth/spreadsheets"]
+        )
+        _gc = gspread.authorize(creds)
         _spreadsheet = _gc.open_by_key(SHEET_ID)
 
     return _spreadsheet.worksheet(sheet_name)
